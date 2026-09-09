2f23b40240a1cd6e6fa08e22542a14a55c0dc88a
Dogan Gunes <dogangns.98@gmail.com>
2026-09-10 01:30:09 +0300

    .

--- lib/products.ts
+++ lib/products.ts
@@ -18,7 +18,7 @@ const catalogue: {id:string;name:string;symbol:Product['symbol']}[] = [
  {id:'odivon-samo-int',name:'OdivonSamoInt',symbol:'integration'},
  {id:'odivon-farm',name:'OdivonFARM',symbol:'farm'},
  {id:'odivon-spa',name:'OdivonSPA',symbol:'spa'},
- {id:'odivon-school',name:'OdivonSchool',symbol:'school'},
+ {id:'odivon-school',name:'OdivonSchool',symbol:'school', demoUrl:'https://school.odivon.com'},
  {id:'odivon-api',name:'OdivonAPI',symbol:'api'},
  {id:'odivon-b2c',name:'OdivonB2C',symbol:'b2c'},
  {id:'odivon-ikimiz',name:'Odivon ikimiz',symbol:'together'},
