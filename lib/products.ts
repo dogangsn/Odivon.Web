e15f742ec618bdb1276db794767351ced605cbc5
Dogan Gunes <dogangns.98@gmail.com>
2026-09-14 17:09:40 +0300

    .

--- lib/products.ts
+++ lib/products.ts
@@ -21,7 +21,7 @@ const catalogue: {id:string;name:string;symbol:Product['symbol']}[] = [
  {id:'odivon-school',name:'OdivonSchool',symbol:'school', demoUrl:'https://school.odivon.com'},
  {id:'odivon-api',name:'OdivonAPI',symbol:'api'},
  {id:'odivon-b2c',name:'OdivonB2C',symbol:'b2c'},
- {id:'odivon-ikimiz',name:'Odivon ikimiz',symbol:'together'},
+ {id:'odivon-ikimiz',name:'Odivon ikimiz',symbol:'together' demoUrl:'https://ikimiz.odivon.com'},
  {id:'odivon-vet',name:'OdivonVET',symbol:'vet', demoUrl:'https://vet.odivon.com'},
 ];
 export const products: Product[] = catalogue.map(product=>({
